53387a7f957ef6d0a07bfa660faf9e66a51574ee
mfreidl <michael.freidl3@edu.fh-joanneum.at>
2022-12-03 23:23:13 +0100

    environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -23,7 +23,7 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.gi
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
